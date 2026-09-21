9f00d1c11debb14eae5a0c91ed3fce2de8064b9d
midlandgara17 <midlandgara17@gmail.com>
2026-09-21 15:39:59 +0300

    Update Config.js
    Environment keys uodated

--- js/Config.js
+++ js/Config.js
@@ -1,3 +1,3 @@
 
 const SUPABASE_URL = "https://jugnkmrsilzbdosacjuy.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0SMZuYiUdo4rrvDaf3YuOA_pz2kCmH9";

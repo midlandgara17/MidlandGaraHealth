12097f69906d0872dd8a301a386d44114b6710fd
midlandgara17 <midlandgara17@gmail.com>
2026-09-21 18:16:03 +0300

    Update Config.js
    Configuration done

--- js/Config.js
+++ js/Config.js
@@ -1,3 +1,7 @@
+window.MGH_CONFIG = {
+  supabaseUrl: "https://jugnkmrsilzbdosacjuy.supabase.co",
+  supabaseAnonKey: "sb_publishable_0SMZuYiUdo4rrvDaf3YuOA_pz2kCmH9"
+};
 
-const SUPABASE_URL = "https://jugnkmrsilzbdosacjuy.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0SMZuYiUdo4rrvDaf3YuOA_pz2kCmH9";
+
+
